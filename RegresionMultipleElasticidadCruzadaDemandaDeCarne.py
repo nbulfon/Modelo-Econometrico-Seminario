4d23b6b4fc19b980d001e00e767761ae41c77d0f
nbulfon <nicolas.bulfon@gmail.com>
2023-05-12 16:56:00 -0300

    Update RegresionMultipleElasticidadCruzadaDemandaDeCarne.py

--- RegresionMultipleElasticidadCruzadaDemandaDeCarne.py
+++ RegresionMultipleElasticidadCruzadaDemandaDeCarne.py
@@ -5,7 +5,13 @@ Created on Thu May 11 23:05:47 2023
 @author: nicol
 """
 
+# Imports
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+# ...
+
 print(pd.__version__)
 
 # Paso 1: cargar datos desde un archivo Excel ->
@@ -40,11 +46,8 @@ print(data)
 
 # Paso 3: ahora voy a hacer la división de los datos en un conjunto de entrenamiento y uno de prueba ->
 
-# importo la biblioteca ->
-from sklearn.model_selection import train_test_split
-
-# dividir los datos en características (X) y variable objetivo (y):
-X = data[['Precio carne aviar', 'Precio carne porcina']]  # Características (precio del pollo y del cerdo)
+# dividir los datos en parámetros (X) y variable objetivo (y):
+X = data[['Precio carne aviar', 'Precio carne porcina']]  # parámetros del modelo (precio del pollo y del cerdo)
 y = data['Consumo carne bovina']  # Variable objetivo (demanda de carne)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
@@ -57,6 +60,20 @@ print('Dimensiones de y_test:', y_test.shape)
 # ...
 
 # Paso 4: Armar el modelo de Regresión múltiple ->
+
+#Crea una instancia del modelo de regresión lineal múltiple:
+data = LinearRegression()
+
+data.fit(X_train, y_train)
+
+# realiza predicciones utilizando el conjunto de prueba:
+y_pred = data.predict(X_test)
+
+#Evalúa el rendimiento del modelo utilizando métricas apropiadas para la regresión,
+# como el error cuadrático medio (MSE) o el coeficiente de determinación (R²).
+# Por ejemplo, para calcular el MSE:
+mse = mean_squared_error(y_test, y_pred)
+
 # ...
 
 
